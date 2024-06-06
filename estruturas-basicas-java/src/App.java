6a75f066d7b6ddafc61a60bdd9ab1da29a5f670d
Stefanie Paes <steefaaniepr@outlook.com>
2024-06-05 23:17:42 -0300

    att

--- estruturas-basicas-java/src/App.java
+++ estruturas-basicas-java/src/App.java
@@ -1,5 +1,19 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+                /* Sintaxe de declaração das variaveis no Java
+                 * <tipo> identificador;
+                 * <tipo> identificador = valor;
+                 * 
+                 * Variaveis tem escopos: blocos de comando no qual
+                 * a variavel foi declarada. 
+                 */
+        
+            String saudacao = "Olá, Stefanie!";
+            System.out.println(saudacao);
+                 
+        }
+
     }
+
 }
