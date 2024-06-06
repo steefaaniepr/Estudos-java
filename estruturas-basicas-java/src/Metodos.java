dd4a35888c568f76e78a9513be902011e724eebd
Stefanie Paes <steefaaniepr@outlook.com>
2024-06-05 23:23:21 -0300

    att

--- estruturas-basicas-java/src/Metodos.java
+++ estruturas-basicas-java/src/Metodos.java
@@ -1,3 +1,17 @@
 public class Metodos {
+   public static void main(String[] args) {
     
+   } 
+
+   /*
+   Sintaxe de declaração de métodos
+
+    * <tipo do retorno do metodo> identificador (<tipo> parâmetro) {
+    <bloco de comando>
+    }
+    */
+
+    void ativarSoneca() {
+        System.out.println("ACORDAR!");
+    }
 }
