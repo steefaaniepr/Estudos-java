21bd64c6df6129a0a6d9370524747fc2a70f0555
Stefanie Paes <steefaaniepr@outlook.com>
2024-06-05 23:09:34 -0300

    commit-inicial

---
+++ variaveis/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+        /* Sintaxe de declaração das variaveis no Java
+         * <tipo> identificador;
+         * <tipo> identificador = valor;
+         * 
+         * Variaveis tem escopos: blocos de comando no qual
+         * a variavel foi declarada. 
+         */
+
+         String saudacao = "Olá, Stefanie!";
+         System.out.println(saudacao);
+         
+    }
+}
