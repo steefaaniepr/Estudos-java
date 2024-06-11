12dc083aeeaed3bfb9cf1c1fd2eb00389338f24b
Stefanie Paes <steefaaniepr@outlook.com>
2024-06-10 23:09:41 -0300

    att

--- estruturas-basicas-java/src/Despertador.java
+++ estruturas-basicas-java/src/Despertador.java
@@ -7,7 +7,7 @@ public class Despertador {
         System.out.println("Só mais " + soneca +  " minutinhos :P");
     }
 
-    String agendarAlarme(int hora, int minutos) { //parâmetros
+    String agendarAlarme() { //parâmetros
         // Seu alarme foi configurado ás 12:30
         return "Seu alarme foi configurado ás " + hora + ":" + minutos;
 
